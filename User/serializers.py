7b3cfe9a211f9c9d436521af2e6055338d401d80
KolyvanovMisha <m.kolyvanov@radugamarket.shop>
2021-10-18 17:51:55 +0500

    added User from api_yamdb

---
+++ User/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from User.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.ChoiceField(choices=User.Role)
+    username = serializers.CharField(
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+            ),
+        ],
+    )
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+            ),
+        ],
+    )
+
+    class Meta:
+        model = User
+
+        fields = (
+            'id', 'username', 'email', 'bio', 'first_name', 'last_name', 'role'
+        )
+
