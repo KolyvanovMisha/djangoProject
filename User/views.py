1befae9829193ee24220658feeba8588f0d045c2
KolyvanovMisha <m.kolyvanov@radugamarket.shop>
2021-10-19 13:42:30 +0500

    setting with mongoengine

--- User/views.py
+++ User/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+
+from User.models import User
 from User.serializers import (UserSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
+    queryset = User.objects.all()
